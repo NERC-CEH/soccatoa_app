6b8ae752c79a57d55db921c30c6989ece136055d
Dave Miller <dave.miller@bioss.ac.uk>
2025-07-11 12:35:03 +0100

    generalize reformat_data (still yucky code)

--- R/utils-others.R
+++ R/utils-others.R
@@ -165,14 +165,28 @@ clean_input <- function(x) {
 #' @return a data.frame that is ready for modelling
 #' @export
 reformat_data <- function(df_loaded) {
-  df <- df_loaded %>%
-    sf::st_as_sf(coords = c("lon", "lat"), crs = 4236) %>%
-    sf::st_transform(27700)
+  # include both northing/easting and lon/lat
+  if(!all(c("northing", "easting") %in% names(df_loaded))){
+    df <- df_loaded %>%
+      sf::st_as_sf(coords = c("lon", "lat"), crs = 4236) %>%
+      sf::st_transform(27700)
+    df <- df %>%
+      dplyr::bind_cols(sf::st_coordinates(df), df_loaded[, c("lon", "lat")]) %>%
+      dplyr::rename(easting = X, northing = Y) %>% # Rename columns
+      sf::st_drop_geometry() # Drop the geometry column
+  }
+  if(!all(c("lon", "lat") %in% names(df_loaded))){
+    df <- df_loaded %>%
+      sf::st_as_sf(coords = c("easting", "northing"), crs = 27700) %>%
+      sf::st_transform(4326)
+    df <- df %>%
+      dplyr::bind_cols(sf::st_coordinates(df),
+                       df_loaded[, c("easting", "northing")]) %>%
+      dplyr::rename(lon = X, lat = Y) %>% # Rename columns
+      sf::st_drop_geometry() # Drop the geometry column
+  }
+
   df %>%
-    # include both northing/easting and lon/lat
-    dplyr::bind_cols(sf::st_coordinates(df), df_loaded[, c("lon", "lat")]) %>%
-    dplyr::rename(easting = X, northing = Y) %>% # Rename columns
-    sf::st_drop_geometry() %>% # Drop the geometry column
     # get carbon density
     dplyr::mutate(rho_c = f_c * rho_fe) %>%
     dplyr::mutate(log_rho_c = log(rho_c), fyear = as.factor(year))
