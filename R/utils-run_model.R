6793d26d42ef31acb49d51ab81eeb84dc6caa2cb
ADCEH\plevy <plevy@ceh.ac.uk>
2025-07-21 17:11:51 +0100

    rename df_loaded & predgrid

--- R/utils-run_model.R
+++ R/utils-run_model.R
@@ -7,28 +7,33 @@
 #' @param n_post_samples how many posterior samples to use
 #' @param n_chains number of chains (just a multiplier on the number of samples
 #' here but when we do Real MCMC we will need use this)
-#' @param df_loaded a `data.frame` of data
-#' @param predgrid prediction grid to use
+#' @param df a `data.frame` of data
+#' @param df_grid prediction grid to use
 #' @return df_results
 #' @importFrom stats coef vcov predict
 #' @importFrom mgcv gam rmvn predict.gam
 #' @importFrom abind abind
 #' @importFrom posterior as_draws_array
 #' @export
-dummy_model <- function(df_loaded, predgrid, n_post_samples, n_chains) {
+dummy_model <- function(df, df_grid, n_post_samples, n_chains) {
+  # check
+  v_year <- unique(df$fyear)
+  if (length(v_year) < 2) {
+    stop("You need at least 2 time points to estimate change")
+  }
   # a very very simple GAM :)
   jg <- mgcv::gam(
     log_rho_c ~
       fyear +
         z,
-    data = df_loaded
+    data = df
   )
 
   # since we're not doing multi-chain MCMC here just generate
   # samples x chains independent samples
   samps <- rmvn(n_post_samples * n_chains, coef(jg), vcov(jg))
 
-  lp <- predict(jg, newdata = predgrid, type = "lpmatrix")
+  lp <- predict(jg, newdata = df_grid, type = "lpmatrix")
   lpp <- lp %*% t(samps)
 
   # put into array format
@@ -44,56 +49,56 @@ dummy_model <- function(df_loaded, predgrid, n_post_samples, n_chains) {
   # give the array some useful names
   dimnames(arr)[[3]] <- paste0("pred[", dimnames(arr)[[3]], "]")
   # return as posterior object
-  posterior::as_draws_array(arr)
+  return(posterior::as_draws_array(arr))
 }
 
 #' Make a prediction grid
 #'
 #' Generate a grid to make predictions over
 #'
-#' @param df_loaded the data.frame of the original data to use to get limits etc
+#' @param df the data.frame of the original data to use to get limits etc
 #' @return a `data.frame` with locations in covariate space to predict at
-make_prediction_grid <- function(df_loaded) {
-  neast <- nnorth <- 20
-  gr <- expand.grid(
+make_prediction_grid <- function(df) {
+  n_east <- n_north <- 20
+  df_grid <- expand.grid(
     easting = seq(
-      min(df_loaded$easting),
-      max(df_loaded$easting),
-      length.out = neast
+      min(df$easting),
+      max(df$easting),
+      length.out = n_east
     ),
     northing = seq(
-      min(df_loaded$northing),
-      max(df_loaded$northing),
-      length.out = nnorth
+      min(df$northing),
+      max(df$northing),
+      length.out = n_north
     ),
     # since we assume log_e rho_c is linear in depth
     # we only need 2 points
     z = c(0.25, 0.55),
     # may want to specify this based on the time
     # period given by the user?
-    fyear = factor(range(df_loaded$year), levels = levels(df_loaded$fyear))
+    fyear = factor(range(df$year), levels = levels(df$fyear))
   )
 
   # these need to be revisited depending on how we change the grid
-  gr$d <- 0.3
-  gr$area <- ((max(df_loaded$easting) - min(df_loaded$easting)) / neast) *
-    (max(df_loaded$northing) - min(df_loaded$northing) / nnorth)
+  df_grid$d <- 0.3
+  df_grid$area <- ((max(df$easting) - min(df$easting)) / n_east) *
+    (max(df$northing) - min(df$northing) / n_north)
   # need to think about whether this is fixed or if
   # marginalize?
-  #S_fez=mean(df_fix$S_fez))
-  gr
+  # S_fez=mean(df_fix$S_fez))
+  return(df_grid)
 }
 
 #' Spatio-temporal model of soil carbon
 #'
 #' This fits our model of soil carbon. At the moment this is just a dummy
 #' function and will eventually call-out to other stuff.
-#' @param df_loaded a `data.frame` of data loaded
+#' @param df a `data.frame` of data loaded
 #' @return df_results
 #' @export
-run_model_A <- function(df_loaded) {
+run_model_A <- function(df) {
   # generate prediction grid
-  predgrid <- make_prediction_grid(df_loaded)
+  df_grid <- make_prediction_grid(df)
 
   ## probably want to define this in the UI later (as an advanced option?)
   # number of posterior samples to generate
@@ -103,10 +108,10 @@ run_model_A <- function(df_loaded) {
   # for the dummy model we just generate n_post_samples*n_chains samples
 
   # this returns 3D array of (iteration) x (chain) x (variable)
-  model_result <- dummy_model(df_loaded, predgrid, n_post_samples, n_chains)
+  model_result <- dummy_model(df, df_grid, n_post_samples, n_chains)
 
   # return both bits
-  list(predgrid = predgrid, results = model_result)
+  list(df_grid = df_grid, results = model_result)
 }
 
 #' Summarize results in a very simple way
@@ -126,7 +131,7 @@ run_model_A <- function(df_loaded) {
 #' @importFrom stats sd quantile
 #' @export
 summarize_results_simple <- function(results, alpha = 0.05) {
-  predgrid <- results$predgrid
+  df_grid <- results$df_grid
   results <- results$results
 
   results <- as_draws_df(results)
@@ -145,7 +150,7 @@ summarize_results_simple <- function(results, alpha = 0.05) {
   results <- do.call(rbind, results)
 
   # bind to prediction data so we can reference to time/space for plots later
-  cbind(predgrid, results)
+  cbind(df_grid, results)
 }
 
 #' Summarize results for graph of changes
@@ -161,7 +166,7 @@ summarize_results_simple <- function(results, alpha = 0.05) {
 #' @importFrom posterior as_draws_df
 #' @importFrom stats median aggregate
 summarize_results_change <- function(results) {
-  predgrid <- results$predgrid
+  df_grid <- results$df_grid
   md <- exp(results$results)
 
   dd <- as_draws_df(md)
@@ -170,9 +175,9 @@ summarize_results_change <- function(results) {
   # apply preserves the iterations
   dd <- apply(dd[, -((ncol(dd) - 2):ncol(dd))], 1, function(x) {
     # get s_ci
-    x <- x * predgrid$d / predgrid$area
+    x <- x * df_grid$d / df_grid$area
     # sum over space per year
-    x <- aggregate(x, list(year = predgrid$fyear), sum)
+    x <- aggregate(x, list(year = df_grid$fyear), sum)
   })
 
   # one row per iteration/year
@@ -198,7 +203,7 @@ summarize_results_change <- function(results) {
 #' @param a `data.frame` with columns:
 #' @export
 summarize_results_dist <- function(results) {
-  predgrid <- results$predgrid
+  df_grid <- results$df_grid
   md <- exp(results$results)
 
   dd <- as_draws_df(md)
@@ -207,9 +212,9 @@ summarize_results_dist <- function(results) {
   # apply preserves the iterations
   dd <- apply(dd[, -((ncol(dd) - 2):ncol(dd))], 1, function(x) {
     # get s_ci
-    x <- x * predgrid$d / predgrid$area
+    x <- x * df_grid$d / df_grid$area
     # sum over space per year
-    x <- aggregate(x, list(year = predgrid$fyear), sum)
+    x <- aggregate(x, list(year = df_grid$fyear), sum)
   })
 
   # one row per iteration/year
@@ -326,7 +331,6 @@ get_co2climate_effect <- function(
 
 #' run_model_B
 #' @description function ...
-#' @param df_loaded a df of data loaded
 #' @param yrstart start year
 #' @param yrend end year
 #' @return df_results
